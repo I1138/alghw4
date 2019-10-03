3f359c2687080ec259d48fb36340ef370c7666e0
Ian Bolin <ianqbolin@cedarville.edu>
2019-10-03 17:20:38 -0400

    Final version, submitted

--- main.py
+++ main.py
@@ -1,47 +1,77 @@
 # algorithms hw 4
+# Ian Bolin and Joel Akers
+# 10/1/19
 import enum
 import random
 
 
+# initializes and tests trees by loading with n random numbers from 0 to 2n and
+#  then deleting. Will detect errors using check tree, and print the tree.
 def main():
     tree = rbTree(10)
+    tree2 = bsTree(10)
     data = []
-    for i in range(10):
-        data.append(random.randint(0, 20))
+    n = 10
+    for i in range(n):
+        data.append(random.randint(0, 2*n))
+        print("inserting ", data[i])
         tree.insert(data[i])
+        tree2.insert(data[i])
+        print("rbTree")
         tree.printTree()
         checkTree(tree, tree.root)
+        print("bsTree")
+        tree2.printTree()
+        checkTree(tree2, tree2.root)
         print()
-    tree.printTree()
-    print(tree.minimum().getData())
-    print(tree.successor(tree.minimum()).getData())
-    print(tree.maximum().getData())
-    print(tree.predecessor(tree.maximum()).getData())
-    checkTree(tree, tree.root)
+    for i in data:
+        print("deleting ", i)
+        deleteNode = tree.search(i)
+        tree.delete(deleteNode)
+        print("rbTree")
+        tree.printTree()
+        checkTree(tree, tree.root)
+        deleteNode = tree2.search(i)
+        tree2.delete(deleteNode)
+        print("bsTree")
+        tree2.printTree()
+        checkTree(tree2, tree2.root)
+        print()
+    print(data)
     return
 
 
+# check tree for errors, and exit on error
 def checkTree(tree, currNode):
+    treeOkay = True
     if currNode == tree.Nil:
         return 1
     if currNode.parent.leftChild != currNode and currNode.parent.rightChild != currNode:
         print("child unclaimed by parent confusion on", currNode.data, "and parent", currNode.parent.data)
+        treeOkay = False
     if currNode.leftChild != tree.Nil and currNode.leftChild.parent != currNode:
         print("left child of parent", currNode.data, "does not recognize parent")
+        treeOkay = False
     if currNode.rightChild != tree.Nil and currNode.rightChild.parent != currNode:
         print("right child of parent", currNode.data, "does not recognize parent")
+        treeOkay = False
     if currNode.getColor() == color.RED and currNode.getParent().getColor() == color.RED:
         print("red node with red parent at ", currNode.data)
+        treeOkay = False
     leftBlackHeight = checkTree(tree, currNode.leftChild)
     rightBlackHeight = checkTree(tree, currNode.rightChild)
-    if leftBlackHeight != rightBlackHeight:
+    if leftBlackHeight != rightBlackHeight and tree is rbTree:
         print("Black heights uneven at", currNode.data)
         # force unevenness up the rest of the tree
         blackHeight = -10
+        treeOkay = False
     else:
         blackHeight = leftBlackHeight
     if currNode.getColor() == color.BLACK:
         blackHeight += 1
+    if not treeOkay:
+        tree.printTree()
+        exit()
     return blackHeight
 
 
@@ -119,7 +149,7 @@ class bsTree:
             else:
                 currNode = currNode.getLeftChild()
         # deal with empty tree
-        if self.Nil.getParent() == self.Nil:
+        if self.root == self.Nil:
             self.root = node(data, self.Nil, self.Nil, self.Nil)
             self.Nil.setLeftChild(self.root)
             self.Nil.setRightChild(self.root)
@@ -157,7 +187,7 @@ class bsTree:
         while currNode.data != value and currNode != self.Nil:
             # value will be less than or greater
             # return None if value is not found before nill node
-            if (currNode.data < value):
+            if (value < currNode.getData()):
                 currNode = currNode.leftChild
             else:
                 currNode = currNode.rightChild
@@ -166,6 +196,7 @@ class bsTree:
             currNode = None
         return currNode
 
+    # print tree using preorder walk with directions
     def printTree(self, indent=" ", currNode=None):
         if currNode is None:
             currNode = self.root
@@ -179,6 +210,8 @@ class bsTree:
     def transplant(self, unplantNode, plantNode):
         if (unplantNode.parent == self.Nil):
             self.root = plantNode
+            self.Nil.leftChild = plantNode
+            self.Nil.rightChild = plantNode
         elif (unplantNode == unplantNode.parent.leftChild):
             unplantNode.parent.leftChild = plantNode
         else:
@@ -244,20 +277,31 @@ class rbTree(bsTree):
             self.printTree(indent+">", currNode.getRightChild())
 
     def insert(self, data):
-        currNode = self.root
-        pastNode = None
-        while currNode != self.Nil:
-            pastNode = currNode
-            if currNode.getData() <= data:
-                currNode = currNode.getRightChild()
+        # handle empty tree case
+        if self.root == self.Nil:
+            insertNode = node(data, self.Nil, self.Nil, self.Nil, color.BLACK)
+            self.root = insertNode
+            self.Nil.setParent(insertNode)
+            self.Nil.setLeftChild(insertNode)
+            self.Nil.setRightChild(insertNode)
+        else:  # standard insert from text
+            currNode = self.root
+            pastNode = None
+            # find insert location
+            while currNode != self.Nil:
+                pastNode = currNode
+                if currNode.getData() <= data:
+                    currNode = currNode.getRightChild()
+                else:
+                    currNode = currNode.getLeftChild()
+            # insert
+            insertNode = node(data, pastNode, self.Nil, self.Nil, color.RED)
+            if data >= pastNode.getData():
+                pastNode.setRightChild(insertNode)
             else:
-                currNode = currNode.getLeftChild()
-        insertNode = node(data, pastNode, self.Nil, self.Nil, color.RED)
-        if pastNode.data <= data:
-            pastNode.setRightChild(insertNode)
-        else:
-            pastNode.setLeftChild(insertNode)
-        self.insertFixUp(insertNode)
+                pastNode.setLeftChild(insertNode)
+            # fixup tree
+            self.insertFixUp(insertNode)
 
     def isRightChild(self, currNode):
         if currNode.parent.rightChild == currNode:
@@ -265,12 +309,12 @@ class rbTree(bsTree):
         return False
 
     def leftRotate(self, pivotNode=None):
-        # set rotateing node
+        # set rotating node
         rotateNode = pivotNode.getRightChild()
 
         # turn rotateNode's left subtree into pivotNode's right subtree
         pivotNode.setRightChild(rotateNode.getLeftChild())
-        if rotateNode != self.Nil:
+        if rotateNode.getLeftChild() != self.Nil:
             rotateNode.leftChild.setParent(pivotNode)
         # link pivotNode's parent to rotateNode
         rotateNode.setParent(pivotNode.getParent())
@@ -294,7 +338,7 @@ class rbTree(bsTree):
         # turn rotateNode's left subtree into pivotNode's right subtree
         pivotNode.setLeftChild(rotateNode.getRightChild())
 
-        if rotateNode != self.Nil:
+        if rotateNode.getRightChild() != self.Nil:
             rotateNode.rightChild.setParent(pivotNode)
 
         # link pivotNode's parent to rotateNode
@@ -346,6 +390,7 @@ class rbTree(bsTree):
                     self.leftRotate(currNode.getParent().getParent())
             self.root.setColor(color.BLACK)
 
+    # removes a NODE from tree. use search to find a node with a given value for deletion
     def delete(self, deleteNode):
         currNode = deleteNode
         currNodeOrigColor = currNode.getColor()
@@ -358,10 +403,10 @@ class rbTree(bsTree):
             self.transplant(deleteNode, deleteNode.getLeftChild())
         else:
             currNode = self.minimum(deleteNode.getRightChild())
-            currNode.setColor(currNodeOrigColor)
+            currNodeOrigColor = currNode.getColor()
             moveNode = currNode.getRightChild()
             if currNode.getParent() == deleteNode:
-                moveNode.getParent() == currNode
+                moveNode.setParent(currNode)
             else:
                 self.transplant(currNode, currNode.getRightChild())
                 currNode.setRightChild(deleteNode.getRightChild())
@@ -376,44 +421,54 @@ class rbTree(bsTree):
     def deleteFixup(self, currNode):
         moveNode = None
         while currNode != self.root and currNode.getColor() == color.BLACK:
-            if not self.isRightChild(currNode):
+            if not self.isRightChild(currNode):  # left side case
                 moveNode = currNode.getParent().getRightChild()
+                # case 1
                 if moveNode.getColor() == color.RED:
                     moveNode.setColor(color.BLACK)
                     currNode.getParent().setColor(color.RED)
                     self.leftRotate(currNode.getParent())
                     moveNode = currNode.getParent().getRightChild()
+                # case 2
                 if moveNode.getLeftChild().getColor() == color.BLACK and moveNode.getRightChild().getColor() == color.BLACK:
                     moveNode.setColor(color.RED)
                     currNode = currNode.getParent()
                 else:
+                    # case 3
                     if moveNode.getRightChild().getColor() == color.BLACK:
                         moveNode.getLeftChild().setColor(color.BLACK)
                         moveNode.setColor(color.RED)
                         self.rightRotate(moveNode)
+                        moveNode = currNode.getParent().getRightChild()
+                    # case 4
                     moveNode.setColor(currNode.getParent().getColor())
                     currNode.getParent().setColor(color.BLACK)
+                    moveNode.getRightChild().setColor(color.BLACK)
                     self.leftRotate(currNode.getParent())
-                    currNode = self.Root
-            else: # TODO: adjust left right
-                moveNode = currNode.getParent().getRightChild()
+                    currNode = self.root
+            else:  # right side cases, same as above, except mirrored
+                moveNode = currNode.getParent().getLeftChild()
                 if moveNode.getColor() == color.RED:
                     moveNode.setColor(color.BLACK)
                     currNode.getParent().setColor(color.RED)
-                    self.leftRotate(currNode.getParent())
-                    moveNode = currNode.getParent().getRightChild()
-                if moveNode.getLeftChild().getColor() == color.BLACK and moveNode.getRightChild().getColor() == color.BLACK:
+                    self.rightRotate(currNode.getParent())
+                    moveNode = currNode.getParent().getLeftChild()
+                if moveNode.getRightChild().getColor() == color.BLACK and moveNode.getLeftChild().getColor() == color.BLACK:
                     moveNode.setColor(color.RED)
                     currNode = currNode.getParent()
                 else:
-                    if moveNode.getRightChild().getColor() == color.BLACK:
-                        moveNode.getLeftChild().setColor(color.BLACK)
+                    if moveNode.getLeftChild().getColor() == color.BLACK:
+                        moveNode.getRightChild().setColor(color.BLACK)
                         moveNode.setColor(color.RED)
-                        self.rightRotate(moveNode)
+                        self.leftRotate(moveNode)
+                        moveNode = currNode.getParent().getLeftChild()
                     moveNode.setColor(currNode.getParent().getColor())
                     currNode.getParent().setColor(color.BLACK)
-                    self.leftRotate(currNode.getParent())
-                    currNode = self.Root
+                    moveNode.getLeftChild().setColor(color.BLACK)
+                    self.rightRotate(currNode.getParent())
+                    currNode = self.root
+        currNode.setColor(color.BLACK)
 
-if __name__ == '__main__':
+
+if __name__ == '__main__':  # run main if not used as library
     main()
